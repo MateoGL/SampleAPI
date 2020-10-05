38ee66dfe3c57fd867dc6e1e250326732f7aacfd
Mateo <mateo.gil@hotmail.com>
2020-10-04 19:28:22 -0500

    Create User Model

---
+++ SampleAPI.Domain/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace SampleAPI.Domain.Models
+{
+    public class User
+    {
+        [Key]
+        public string Username { get; set; }
+        public string Email1 { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime? CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
